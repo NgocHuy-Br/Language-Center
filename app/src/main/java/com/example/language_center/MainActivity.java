056818f37aa07bb7b0d92286734d024a0e5f0f17
Huy Cao <ngochuy.mep@gmail.com>
2026-04-19 21:15:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/language_center/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.language_center;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edAccount, edPassword;
+    Button btLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Mapping
+        edAccount = (EditText) findViewById(R.id.editTextAccount);
+        edPassword = (EditText) findViewById(R.id.editTextPassword);
+        btLogin = (Button) findViewById(R.id.buttonLogin);
+
+        // Event handle
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edAccount.getText().toString();
+                String password = edPassword.getText().toString();
+
+                if ((user.equals("K24DTCN258")) && (password.equals("12345"))) {
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this, Welcome.class);
+                    intent.putExtra("user_name", user);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/language_center/Welcome.java
@@ -0,0 +1,37 @@
+package com.example.language_center;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Welcome extends AppCompatActivity {
+    TextView tvWelcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // mapping
+        tvWelcome = (TextView) findViewById(R.id.textViewWelcome);
+
+        // getIntent
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("user_name");
+
+        // setText
+        tvWelcome.setText("Xin chào bạn " + userName);
+    }
+}
